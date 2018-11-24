3fb78dbb9ac411bdbfa56487d56c1f4c26b5db8e
by-ktif <monovar@tut.by>
2018-11-24 19:12:31 +0100

    validate location

--- server/src/main/java/pl/hackyeah/colorando/server/game/GameService.java
+++ server/src/main/java/pl/hackyeah/colorando/server/game/GameService.java
@@ -10,4 +10,12 @@ public class GameService {
         gamesById.put(newGame.getUuid(), newGame);
         return newGame.getUuid();
     }
+
+    public boolean validateGameStart(String uuid, String deviceLocation) {
+        return validateLocation(gamesById.get(uuid).getLocation(), deviceLocation);
+    }
+
+    private boolean validateLocation(String location, String deviceLocation) {
+        return location.equals(deviceLocation); // we may want to implement some margin allowance logic here
+    }
 }
