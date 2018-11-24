90de43b06d79fbc81d4dcc506aefc0f479074a36
by-ktif <monovar@tut.by>
2018-11-24 20:37:24 +0100

    User Service draft concept

--- server/src/main/java/pl/hackyeah/colorando/server/qrapi/QRCodeController.java
+++ server/src/main/java/pl/hackyeah/colorando/server/qrapi/QRCodeController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.hackyeah.colorando.server.game.GameService;
+import pl.hackyeah.colorando.server.user.UserService;
 
 @RestController
 class QRCodeController {
 
     @Autowired
     GameService gameService;
+    
+    @Autowired
+    UserService userService;
 
     @GetMapping("/qrcode")
     public String getQrCode(String location) {
@@ -19,11 +23,21 @@ class QRCodeController {
 
     @PostMapping("/qrcode")
     public Boolean postScannedQrCode(String uuid, String location) {
-        return gameService.validateGameStart(uuid, location);
+        userService.authenticateUser(/*some attributes*/);
+        boolean validationResult = gameService.validateGameStart(uuid, location);
+        if (validationResult) {
+            userService.withdrawMoney(/*some attributes*/);
+        }
+        return validationResult;
     }
 
     @PostMapping("/play")
     public Boolean postUserGuess(String uuid, String guessAttempt) {
-        return gameService.validateGuess(uuid, guessAttempt);
+        userService.authenticateUser(/*some attributes*/);
+        boolean validationResult = gameService.validateGuess(uuid, guessAttempt);
+        if (validationResult) {
+            userService.payWinningMoney(/*some attributes*/);
+        }
+        return validationResult;
     }
 }
---
+++ server/src/main/java/pl/hackyeah/colorando/server/user/UserService.java
@@ -0,0 +1,20 @@
+package pl.hackyeah.colorando.server.user;
+
+public class UserService {
+
+    public void authenticateUser(/* some attributes */) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void payWinningMoney(/* some attributes */) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void withdrawMoney(/* some attributes */) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
